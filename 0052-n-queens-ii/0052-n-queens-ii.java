8d346bedbce82c9d521c7d37024df1e0713439a8
Kartikey000111 <devdubey226@gmail.com>
2026-07-12 23:33:43 +0530

    Time: 2 ms (64.36%), Space: 42.1 MB (45.21%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,55 @@
+class Solution {
+    private static int nQueens(int row, char[][] chessBoard) {
+        int n= chessBoard.length;
+        if(row==n){
+            return 1;
+        }
+        int count=0;
+        for(int col=0;col<n;col++){
+            if(canQueensbePlaced(row,col,chessBoard)){
+                chessBoard[row][col]='Q';
+                count+=nQueens(row+1,chessBoard);
+                chessBoard[row][col]='i';
+            }
+        }
+        return count;
+    }
+
+    private static boolean canQueensbePlaced(int row, int col, char[][] chessBoard) {
+        int n = chessBoard.length;
+        int i = row - 1;
+       
+        while (i >= 0) {
+            if (chessBoard[i][col] == 'Q') return false;
+            i--;
+        }
+       
+        i = row - 1;
+        int j = col - 1;
+        while (i >= 0 && j >= 0) {
+            if (chessBoard[i][j] == 'Q') return false;
+            i--;
+            j--;
+        }
+ 
+        i = row - 1;
+        j = col + 1;
+        while (i >= 0 && j < n) {
+            if (chessBoard[i][j] == 'Q') return false;
+            i--;
+            j++;
+        }
+        return true;
+    }
+    public int totalNQueens(int n) {
+        
+        char[][] chessBoard=new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                chessBoard[i][j]='i';
+            }
+        }
+        int ans=nQueens(0,chessBoard);
+        return ans;
+    }
+}
